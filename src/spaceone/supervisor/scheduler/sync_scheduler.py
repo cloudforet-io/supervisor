49c2ef160dd7448efae80073220eded6547e8a7d
Jongmin Kim <whdalsrnt@megazone.com>
2023-05-03 11:34:47 +0900

    refactor: remove worker when syncing plugins

--- src/spaceone/supervisor/scheduler/sync_scheduler.py
+++ src/spaceone/supervisor/scheduler/sync_scheduler.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 #
 #   Copyright 2020 The SpaceONE Authors.
 #
@@ -13,15 +12,14 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-
-import consul
+import copy
 import logging
-import time
 
 from spaceone.core import config
-from spaceone.core.error import ERROR_CONFIGURATION
+from spaceone.core.error import ERROR_CONFIGURATION, ERROR_UNKNOWN
 from spaceone.core.scheduler.scheduler import IntervalScheduler
 from spaceone.core.auth.jwt.jwt_util import JWTUtil
+from spaceone.supervisor.service.supervisor_service import SupervisorService
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -34,122 +32,54 @@ def _get_domain_id_from_token(token):
     return decoded_token['did']
 
 
-WAIT_QUEUE_INITIALIZED = 10     # seconds for waiting queue initilization
-INTERVAL = 10
-MAX_COUNT = 10
-
-
-def _validate_token(token):
-    if isinstance(token, dict):
-        protocol = token['protocol']
-        if protocol == 'consul':
-            consul_instance = Consul(token['config'])
-            value = False
-            while value is False:
-                uri = token['uri']
-                value = consul_instance.patch_token(uri)
-                if value:
-                    _LOGGER.warn(f'[_validate_token] token: {value[:30]} uri: {uri}')
-                    break
-                time.sleep(INTERVAL)
-
-            token = value
-
-    return token
-
-
 class SyncScheduler(IntervalScheduler):
     """ SyncScheduler
     """
-    def check_global_configuration(self):
+    @staticmethod
+    def get_task_metadata_and_params():
         try:
-            self.name = config.get_global('NAME')
-            self.hostname = config.get_global('HOSTNAME')
-            self.tags = config.get_global('TAGS', {})
-            self.labels = config.get_global('LABELS', {})
-            self.plugin_config = config.get_global('PLUGIN')
-            self.token = config.get_global('TOKEN')
-            if self.token == "":
-                self.token = _validate_token(config.get_global('TOKEN_INFO'))
+            name = config.get_global('NAME', '')
+            hostname = config.get_global('HOSTNAME', '')
+            token = config.get_global('TOKEN', '')
 
-            if self.token == "":
+            if token == '':
                 _LOGGER.error("TOKEN is not configured")
                 raise ERROR_CONFIGURATION(key='TOKEN')
-            if self.name == "":
+
+            if name == '':
                 _LOGGER.error("name is not configured!")
                 raise ERROR_CONFIGURATION(key='NAME')
-            if self.hostname == "":
+
+            if hostname == '':
                 _LOGGER.error("hostname is not configured!")
                 raise ERROR_CONFIGURATION(key='HOSTNAME')
 
-            self.domain_id = _get_domain_id_from_token(self.token)
-            return True
+            tags = config.get_global('TAGS', {})
+            labels = config.get_global('LABELS', {})
+            domain_id = _get_domain_id_from_token(token)
+
+            metadata = {
+                'token': token,
+                'domain_id': domain_id
+            }
+
+            params = {
+                'name': name,
+                'hostname': hostname,
+                'tags': tags,
+                'labels': labels,
+                'domain_id': domain_id
+            }
+
+            return metadata, params
+
         except Exception as e:
             _LOGGER.error(f'[check_global_configuration] error: {e}', exc_info=True)
-            return False
+            raise ERROR_UNKNOWN(message=f'[check_global_configuration] error: {e}')
 
     def create_task(self):
-        check = self.check_global_configuration()
-        if check == False:
-            return []
+        metadata, params = self.get_task_metadata_and_params()
+        supervisor_svc: SupervisorService = SupervisorService(metadata)
+        supervisor_svc.sync_plugins(copy.deepcopy(params))
 
-        metadata = {'token': self.token, 'domain_id': self.domain_id}
-        sync_task = {
-            'locator': 'SERVICE',
-            'name': 'SupervisorService',
-            'metadata': metadata,
-            'method': 'sync_plugins',
-            'params': {'params': {
-                            'name': self.name,
-                            'hostname': self.hostname,
-                            'tags': self.tags,
-                            'labels': self.labels,
-                            'domain_id': self.domain_id
-                            }
-                       }
-        }
-
-        #return [{'stages': [sync_task, publish_task]}]
-        return [{'stages': [sync_task]}]
-
-
-class Consul:
-    def __init__(self, config):
-        """
-        Args:
-          - config: connection parameter
-
-        Example:
-            config = {
-                    'host': 'consul.example.com',
-                    'port': 8500
-                }
-        """
-        self.config = self._validate_config(config)
-
-    def _validate_config(self, config):
-        """
-        Parameter for Consul
-        - host, port=8500, token=None, scheme=http, consistency=default, dc=None, verify=True, cert=None
-        """
-        options = ['host', 'port', 'token', 'scheme', 'consistency', 'dc', 'verify', 'cert']
-        result = {}
-        for item in options:
-            value = config.get(item, None)
-            if value:
-              result[item] = value
-        return result
-
-    def patch_token(self, key):
-        """
-        Args:
-            key: Query key (ex. /debug/supervisor/TOKEN)
-
-        """
-        try:
-            conn = consul.Consul(**self.config)
-            index, data = conn.kv.get(key)
-            return data['Value'].decode('ascii')
-
-        except Exception as e:
-            return False
+        return []
