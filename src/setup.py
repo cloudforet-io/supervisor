e8128946be4e8ff6881538352d88c7f4fab717fa
jinyoungmoonDEV <97270590+jinyoungmoonDEV@users.noreply.github.com>
2025-11-14 13:40:52 +0900

    fix: PYPI 정책 변경으로 인한 수정
    Signed-off-by: jinyoungmoonDEV <97270590+jinyoungmoonDEV@users.noreply.github.com>

--- src/setup.py
+++ src/setup.py
@@ -17,7 +17,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    name="spaceone-supervisor",
+    name="spaceone_supervisor",
     version=os.environ.get("PACKAGE_VERSION"),
     description="SpaceONE supervisor service",
     long_description="",
